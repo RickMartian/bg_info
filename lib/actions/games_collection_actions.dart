9b365db6677e3e20e661608f0542f1cb20af3e2a
RickMartian <rikmarciano@outlook.com.br>
2020-05-24 18:25:20 -0300

    #08 - adding handle error to game search

--- lib/actions/games_collection_actions.dart
+++ lib/actions/games_collection_actions.dart
@@ -21,37 +21,52 @@ dynamic fetchThingsFromBgg(dynamic id) async {
 
 dynamic fetchBoardGamesFromBgg(String text, BuildContext context) async {
   print("TEXTO A SER BUSCADO -> $text");
+  final pages = Provider.of<Pages>(context, listen: false);
+  pages.updateIsLoading(true);
   final url = "${Config.baseBggUrl}/search?query=$text";
   final response = await http.get(url);
   final myTransformer = Xml2Json();
-  final pages = Provider.of<Pages>(context, listen: false);
-
-  pages.updateIsLoading(true);
 
   if (response.statusCode == 200) {
     List<Map<String, dynamic>> listOfGamesFetched = [];
     myTransformer.parse(response.body);
     final json = myTransformer.toGData();
-    Map<String, dynamic> decoded = convert.jsonDecode(json);
-    List<dynamic> array = decoded['items']['item'];
-    if (array != null) {
-      for (var value in array) {
-        final type = value["type"];
-        if (type == 'boardgame' || type == 'boardgameexpansion') {
-          final id = value["id"];
-          final title = value["name"]["value"];
-          await fetchThingsFromBgg(id).then((response) {
-            final image = response == null
-                ? "https://www.publicdomainpictures.net/pictures/280000/velka/not-found-image-15383864787lu.jpg"
-                : response;
-            listOfGamesFetched
-                .add({"id": id, "title": title, "image": image, "type": type});
-          });
+    try {
+      Map<String, dynamic> decoded = convert.jsonDecode(json);
+      List<dynamic> array = decoded['items']['item'];
+      if (array != null) {
+        for (var value in array) {
+          final type = value["type"];
+          if (type == 'boardgame' || type == 'boardgameexpansion') {
+            final id = value["id"];
+            final title = value["name"]["value"];
+            await fetchThingsFromBgg(id).then((response) {
+              final image = response == null
+                  ? "https://www.publicdomainpictures.net/pictures/280000/velka/not-found-image-15383864787lu.jpg"
+                  : response;
+              listOfGamesFetched.add(
+                  {"id": id, "title": title, "image": image, "type": type});
+            });
+          }
         }
       }
+      pages.updateSearchListOfThingsFromBgg(listOfGamesFetched);
+      pages.updateIsLoading(false);
+      return listOfGamesFetched;
+    } catch (error) {
+      print("json parse error -> $error");
+      pages.updateIsLoading(false);
+      pages.changeNeedToOpenCollectionPageModal(false);
+      Navigator.popUntil(context, (route) => route.isFirst);
+      final snackBar = SnackBar(
+        content: Text(
+          "Ocorreu um erro ao buscar o jogo :(\nTente especificar melhor o nome :D",
+        ),
+        duration: Duration(
+          milliseconds: 3500,
+        ),
+      );
+      pages.scaffoldKey.currentState.showSnackBar(snackBar);
     }
-    pages.updateSearchListOfThingsFromBgg(listOfGamesFetched);
-    pages.updateIsLoading(false);
-    return listOfGamesFetched;
   }
 }
--- lib/utils/details_card_text.dart
+++ lib/utils/details_card_text.dart
@@ -1,23 +1,25 @@
 import 'package:flutter/material.dart';
 
 Widget renderDetailsText(title, text, align) {
-  return RichText(
-    textAlign: align,
-    text: TextSpan(
-      style: TextStyle(
-        color: Colors.white,
-      ),
-      children: [
-        TextSpan(
-          text: title,
-          style: TextStyle(
-            fontWeight: FontWeight.bold,
+  return Flexible(
+    child: RichText(
+      textAlign: align,
+      text: TextSpan(
+        style: TextStyle(
+          color: Colors.white,
+        ),
+        children: [
+          TextSpan(
+            text: title,
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+            ),
           ),
-        ),
-        TextSpan(
-          text: text,
-        ),
-      ],
+          TextSpan(
+            text: text,
+          ),
+        ],
+      ),
     ),
   );
 }
